45653ba0a8ce795531b7b933d30a6c392e28b917
‘Andylau89’ <332018763@qq.com>
2020-02-18 21:51:00 +0800

    add header

--- Spider/code01.py
+++ Spider/code01.py
@@ -1,14 +1,16 @@
 # 导入请求模块（python标准库）
-import urllib.request
+from urllib import request
 
 # 网址必须写完全，包括http（不是https）
 url = 'http://www.sina.com.cn/'
 url = 'http://www.eastmoney.com/'
 url = 'http://httpbin.org/get'  #查看请求头
 
-# header = 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; InfoPath.3)'
+header = {'User-Agent':'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; InfoPath.3)'}
 # 向百度发请求，得到响应对象
-response = urllib.request.urlopen(url)
+req = request.Request(url = url,headers = header)
+
+response = request.urlopen(req)
 # print(response)
 # print(type(response))
 # 获取相应对象内容（网页源代码）
