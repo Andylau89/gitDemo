22abf61a5b9ae6b47b0d884d45bc7371d448c921
‘Andylau89’ <332018763@qq.com>
2020-02-20 13:06:31 +0800

    用类class实现spider的步骤思想

---
+++ Spider/exercise_百度贴吧_class.py
@@ -0,0 +1,27 @@
+from urllib import request, parse
+
+
+class BaiduSpider(object):
+    def __init__(self):
+        pass
+
+    # 获取响应1
+    def get_page(self):
+        pass
+
+    # 提取数据2
+    def parse_page(self):
+        pass
+
+    # 保存数据3
+    def write_page(self):
+        pass
+
+    # 主函数
+    def main(self):
+        pass
+
+if __name__ == '__main__':
+    spider = BaiduSpider()
+    spider.main()
+
